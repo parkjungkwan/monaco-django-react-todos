fdfde4b31fbfbe3c9213c3c1bd39e890648c0ff1
pakjkwan <pakjkwan@gmail.com>
2021-06-04 13:01:46 +0900

    13

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Signup from './user/SignUp'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Signup />
   </React.StrictMode>,
   document.getElementById('root')
 );
